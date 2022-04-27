73539ac985ad40fbd49f7e3966896a9aa3536f70
mauricio <mauriciocon@gmail.com>
2022-04-27 14:02:33 -0300

    Fix: database.py

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,9 +1,19 @@
-import imp
-from pyexpat import model
-from sqlmodel import create_engine
-from beerlog.config import settings
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlalchemy.sql.expression import Select, SelectOfScalar
+
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+
+
+from sqlmodel import create_engine, Session
+
 from beerlog import models
+from beerlog.config import settings
 
-engine = create_engine(settings.database.url)
-
+engine = create_engine(settings.database.url, echo=False)
 models.SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
