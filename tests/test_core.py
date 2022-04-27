d909289597c7ae4bcc10a2c6c378b9feaf9dd8b0
mauricio <mauriciocon@gmail.com>
2022-04-27 15:18:43 -0300

    Fix: test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,5 +6,6 @@ def test_add_beer_to_database():
 
 
 def test_get_beers_from_database():
+    add_beer_to_database("Blue Moon", "Witbier", 10, 3, 6)  # NEW
     results = get_beers_from_database()
     assert len(results) > 0
