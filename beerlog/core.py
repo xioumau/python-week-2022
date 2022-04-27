fc95065dbc2918f66b884edbc9eddcd380782f00
mauricio <mauriciocon@gmail.com>
2022-04-27 10:05:30 -0300

    Add: core.py

--- beerlog/core.py
+++ beerlog/core.py
@@ -1,8 +1,28 @@
-from typing import Optional
+from re import S
+from typing import Optional, List
 from sqlmodel import select
+from beerlog.database import get_session
+from beerlog.models import Beer
 
 
-def add_beer_to_database() -> bool:
+def add_beer_to_database(
+    name: str,
+    style: str,
+    flavor: int,
+    image: int,
+    cost: int,
+) -> bool:
     with get_session() as session:
+        beer = Beer(**locals())
+        session.add(beer)
+        session.commit()
 
     return True
+
+
+def get_beers_from_database(style: Optional[str] = None) -> List[Beer]:
+    with get_session() as session:
+        sql = select(Beer)
+        if style:
+            sql = select.where(Beer.style == style)
+        return List(session.exec(sql))
