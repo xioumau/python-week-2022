d2991c32aacb335e62fe851253055c5d00272ae4
mauricio <mauriciocon@gmail.com>
2022-04-27 12:01:18 -0300

    Add: serializers.py

---
+++ beerlog/serializers.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from pydantic import BaseModel, validator
+from fastapi import HTTPSException, status
+
+
+class BeerOut(BaseModel):
+    id: int
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+    rate: int
+    date: datetime
+
+
+class BeerIn(BaseModel):
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+
+    @validator("flavor", "image", "cost")
+    def validate_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise HTTPSException(
+                details=f"{field.name} must be between 1 and 10.",
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+            return v
